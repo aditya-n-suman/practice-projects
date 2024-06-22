ecac238907ed40e466730db0e5c352ad2131ca15
Aditya Nath Suman <adityanathsuman13@gmail.com>
2024-06-22 19:44:04 +0530

    vanillaJs-web-api: update video lapsed timer

--- vanillaJs-web-api/script.js
+++ vanillaJs-web-api/script.js
@@ -1,42 +1,63 @@
-const video = document.getElementById("video")
-const playpause = document.getElementById("play")
-const stopBtn = document.getElementById("stop")
-const timestamp = document.getElementById("timestamp")
-const progress = document.getElementById("progress")
+const video = document.getElementById("video");
+const playpause = document.getElementById("play");
+const stopBtn = document.getElementById("stop");
+const timestamp = document.getElementById("timestamp");
+const progress = document.getElementById("progress");
 
 video.controls = false;
 
 const clickPlaypause = () => {
-  if(video.paused || video.ended) {
-    video.play();
-    document.getElementsByClassName('fa-play')[0].style.display = "none"
-    document.getElementsByClassName('fa-pause')[0].style.display = "block"
-  } else {
-    video.pause();
-    document.getElementsByClassName('fa-pause')[0].style.display = "none"
-    document.getElementsByClassName('fa-play')[0].style.display = "block"
-  }
-}
+	if (video.paused || video.ended) {
+		video.play();
+		document.getElementsByClassName("fa-play")[0].style.display = "none";
+		document.getElementsByClassName("fa-pause")[0].style.display = "block";
+	} else {
+		video.pause();
+		document.getElementsByClassName("fa-pause")[0].style.display = "none";
+		document.getElementsByClassName("fa-play")[0].style.display = "block";
+	}
+};
 const clickStop = () => {
-  video.pause();
-  video.currentTime = 0;
-  progress.value = 0;
-}
+	video.pause();
+	video.currentTime = 0;
+	progress.value = 0;
+};
 const seekProgress = (e) => {
+	const rect = progress.getBoundingClientRect();
+	const pos = (e.pageX - rect.left) / progress.offsetWidth;
+	video.currentTime = pos * video.duration;
+};
 
-}
+const formattedTime = (time, isHourLong) => {
+	const inSeconds = Math.ceil(time);
+	const hours = Math.ceil(inSeconds / 3600);
+	let remainder = inSeconds % 3600;
+	const minutes = Math.floor(remainder / 60);
+	const seconds = Math.floor(remainder % 60);
 
-playpause.addEventListener('click', clickPlaypause);
-stopBtn.addEventListener('click', clickStop);
+	const hourStr = `${hours < 10 ? "0" : ""}${hours}`;
 
-video.addEventListener("timeupdate", () => {
-  if(!progress.getAttribute("max")) {
-    progress.setAttribute("max", video.duration)
-  }
-  progress.value = video.currentTime;
-})
-progress.addEventListener("click", (e) => {
-  const rect = progress.getBoundingClientRect();
-  const pos = (e.pageX - rect.left) / progress.offsetWidth;
-  video.currentTime = pos * video.duration;
-})
+	return `${isHourLong ? hourStr + ":" : ""}${
+		minutes < 10 ? "0" : ""
+	}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+};
+
+const timeUpdateListener = () => {
+	if (!progress.getAttribute("max")) {
+		progress.setAttribute("max", video.duration);
+		console.log("duration is", video.duration);
+	}
+	progress.value = video.currentTime;
+	timestamp.innerText = formattedTime(
+		video.currentTime,
+		video.duration >= 3600
+	);
+};
+
+playpause.addEventListener("click", clickPlaypause);
+stopBtn.addEventListener("click", clickStop);
+video.addEventListener("click", clickPlaypause);
+video.addEventListener("playing", (e) => {});
+
+video.addEventListener("timeupdate", timeUpdateListener);
+progress.addEventListener("click", seekProgress);
